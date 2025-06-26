1c15fe4313b5475a8a212beb20c333a9dfe2c08e
Timothy Julian <timothyjulian2001@yahoo.co.id>
2025-06-26 23:51:24 +0700

    skeleton

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+
+mod todo;
+mod template;
+
+use axum::{response::IntoResponse, routing::get};
+use tokio::net::TcpListener;
+
+use crate::template::{HtmlTemplate, IndexTemplate};
+
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    let app = axum::Router::new()
+    .route("/", get(index));
+    let listener = TcpListener::bind("127.0.0.1:3000").await?;
+    println!("listening on {}", listener.local_addr().unwrap());
+
+    axum::serve(listener, app).await?;
+    Ok(())
+}
+
+async fn index() -> impl IntoResponse {
+    // return HtmlTemplate to make it a valid responese
+    HtmlTemplate(IndexTemplate {}) 
 }
---
+++ src/template.rs
@@ -0,0 +1,20 @@
+use askama::Template;
+use axum::{http::StatusCode, response::{Html, IntoResponse}};
+
+#[derive(Template)]
+#[template(path="index.html")]
+pub struct IndexTemplate{}
+
+pub struct HtmlTemplate<T>(pub T);
+
+impl <T> IntoResponse for HtmlTemplate<T> where T: Template {
+    fn into_response(self) -> axum::response::Response {
+        match self.0.render() {
+            Ok(html) => Html(html).into_response(),
+            Err(err) => (
+                StatusCode::INTERNAL_SERVER_ERROR,
+                format!("Failed to render template. Error {}", err),
+            ).into_response(),
+        }
+    }
+}
